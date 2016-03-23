da0d47cce6b40f470776e4849ea2ae7dbe93756f
Michael Luo <deltameter@gmail.com>
2016-03-22 21:43:49 -0400

    fucked up other commit

--- api/temp/8dd1fc44d251be9ac8ee/Main.java
+++
@@ -1,9 +0,0 @@
-import org.junit.*; 
-
-public class Main{
-	public static void main(String[] args){
-		System.out.println("Colin Murphy is a cuck.");
-		System.out.println("Colin Murphy is going to fail Linear Algebra");
-		System.out.println("It's fun to stay at the YMCA");
-	}
-}
